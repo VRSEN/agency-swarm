41865e4864ec29640a3b4ff0fda6bcb6be2b8e67
Arsenii Shatokhin <arseny9795@gmail.com>
2024-01-09 08:07:14 +0400

    Possible fix with screenshots on windows #36

--- agency_swarm/tools/browsing/util/get_b64_screenshot.py
+++ agency_swarm/tools/browsing/util/get_b64_screenshot.py
@@ -1,20 +1,23 @@
 import base64
+import os
 import tempfile
 
 
 def get_b64_screenshot(wd, element=None):
-    tmpfile = tempfile.NamedTemporaryFile(suffix='.png')
+    # Create a temporary file name but don't open it
+    _, tmpfile_name = tempfile.mkstemp(suffix='.png')
 
-    if element:
-        element.screenshot(tmpfile.name)
-    else:
-        wd.get_screenshot_as_file(tmpfile.name)
+    try:
+        if element:
+            element.screenshot(tmpfile_name)
+        else:
+            wd.get_screenshot_as_file(tmpfile_name)
 
-    tmpfile.seek(0)
+        with open(tmpfile_name, 'rb') as f:
+            screenshot = f.read()
+        screenshot_b64 = base64.b64encode(screenshot).decode()
 
-    screenshot = tmpfile.read()
-    screenshot = base64.b64encode(screenshot).decode()
-
-    tmpfile.close()
-
-    return screenshot
+        return screenshot_b64
+    finally:
+        # Clean up the temporary file
+        os.remove(tmpfile_name)
