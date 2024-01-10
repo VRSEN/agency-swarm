128596e248e504115055103d83ac9df8a41841b5
Arsenii Shatokhin <arseny9795@gmail.com>
2024-01-10 10:40:21 +0400

    Fix #39

--- agency_swarm/tools/browsing/util/get_b64_screenshot.py
+++ agency_swarm/tools/browsing/util/get_b64_screenshot.py
@@ -1,7 +1,7 @@
 
 def get_b64_screenshot(wd, element=None):
     if element:
-        screenshot_b64 = element.get_screenshot_as_base64()
+        screenshot_b64 = element.screenshot_as_base64
     else:
         screenshot_b64 = wd.get_screenshot_as_base64()
 
