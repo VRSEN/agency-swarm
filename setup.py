d096a67178d582a2d0c4152b710ac32995172d55
Arsenii Shatokhin <arseny9795@gmail.com>
2024-01-19 08:03:06 +0400

    Added pypi workflow

--- setup.py
+++ setup.py
@@ -9,25 +9,19 @@ setup(
     version='0.1.0',
     author='VRSEN',
     author_email='arseny9795@gmail.com',
-    description='Replace your own agency with an agent swarm.',
+    description='An opensource agent orchestration frameworks built on top of the latest OpenAI Assistants API.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/VRSEN/agency-swarm',
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        # Classifiers help users find your project by categorizing it
-        'Development Status :: 3 - Alpha',  # Change as appropriate
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',  # Choose the appropriate license
-        'Programming Language :: Python :: 3',  # Specify which pyhton versions you support
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
     ],
     entry_points = {
         'console_scripts': ['agency-swarm=agency_swarm.cli:main'],
     },
-    python_requires='>=3.7',  # Specify the Python version requirements
+    python_requires='>=3.7',
 )
