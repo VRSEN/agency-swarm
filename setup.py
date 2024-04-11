194c0e6ef89ccc7a322d1906a2d0c98521c290bd
Arsenii Shatokhin <arseny9795@gmail.com>
2024-04-11 09:00:21 +0400

    Exclude tests dir

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/VRSEN/agency-swarm',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=requirements,
     classifiers=[
         'Intended Audience :: Developers',
