0e65ae1cb6c78799cc3ea88a329e609c339eb991
Arsenii Shatokhin <arseny9795@gmail.com>
2024-08-15 09:09:05 +0400

    Removed dependency on instructor 🥲

--- agency_swarm/tools/BaseTool.py
+++ agency_swarm/tools/BaseTool.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Any, ClassVar
+from typing import Any, ClassVar
 
-from instructor import OpenAISchema
+from docstring_parser import parse
 
-from pydantic import Field
+from pydantic import BaseModel
 from agency_swarm.util.shared_state import SharedState
 
 
-class BaseTool(OpenAISchema, ABC):
+class BaseTool(BaseModel, ABC):
     shared_state: ClassVar[SharedState] = None
     caller_agent: Any = None
     event_handler: Any = None
@@ -24,7 +24,45 @@ class BaseTool(OpenAISchema, ABC):
     @classmethod
     @property
     def openai_schema(cls):
-        schema = super(BaseTool, cls).openai_schema
+        """
+        Return the schema in the format of OpenAI's schema as jsonschema
+
+        Note:
+            Its important to add a docstring to describe how to best use this class, it will be included in the description attribute and be part of the prompt.
+
+        Returns:
+            model_json_schema (dict): A dictionary in the format of OpenAI's schema as jsonschema
+        """
+        schema = cls.model_json_schema()
+        docstring = parse(cls.__doc__ or "")
+        parameters = {
+            k: v for k, v in schema.items() if k not in ("title", "description")
+        }
+        for param in docstring.params:
+            if (name := param.arg_name) in parameters["properties"] and (
+                description := param.description
+            ):
+                if "description" not in parameters["properties"][name]:
+                    parameters["properties"][name]["description"] = description
+
+        parameters["required"] = sorted(
+            k for k, v in parameters["properties"].items() if "default" not in v
+        )
+
+        if "description" not in schema:
+            if docstring.short_description:
+                schema["description"] = docstring.short_description
+            else:
+                schema["description"] = (
+                    f"Correctly extracted `{cls.__name__}` with all "
+                    f"the required parameters with correct types"
+                )
+
+        schema = {
+            "name": schema["title"],
+            "description": schema["description"],
+            "parameters": parameters,
+        }
 
         properties = schema.get("parameters", {}).get("properties", {})
 
