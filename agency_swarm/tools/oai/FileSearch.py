5c23a06a6ba1029fc5ca35286437f830525b4963
Arsenii Shatokhin <arseny9795@gmail.com>
2024-09-21 08:56:09 +0400

    Added ranking options to file search

--- agency_swarm/tools/oai/FileSearch.py
+++ agency_swarm/tools/oai/FileSearch.py
@@ -1,8 +1,13 @@
 from typing import Optional
 from pydantic import BaseModel, field_validator, Field
+from typing import Dict, Union, Optional
 
 class FileSearchConfig(BaseModel):
     max_num_results: int = Field(50, description="Optional override for the maximum number of results")
+    ranking_options: Optional[Dict[str, Union[str, float]]] = Field(
+        {'ranker': 'default_2024_08_21', 'score_threshold': 0.0},
+        description="The ranking options for the file search. If not specified, the file search tool will use the auto ranker and a score_threshold of 0."
+    )
 
     @field_validator('max_num_results')
     def check_max_num_results(cls, v):
