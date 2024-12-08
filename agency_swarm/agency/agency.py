fc95c43d4e37cf98ddd288252b80d4f07b59f537
Nick Bobrowski <39348559+bonk1t@users.noreply.github.com>
2024-12-08 16:28:50 +0000

    Add a missing argument

--- agency_swarm/agency/agency.py
+++ agency_swarm/agency/agency.py
@@ -757,7 +757,7 @@ class Agency:
             dropdown.change(handle_dropdown_change, dropdown)
             file_upload.change(handle_file_upload, file_upload)
             msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
-                bot, [msg, chatbot], [msg, chatbot, dropdown]
+                bot, [msg, chatbot, dropdown], [msg, chatbot, dropdown]
             )
 
             # Enable queuing for streaming intermediate outputs
