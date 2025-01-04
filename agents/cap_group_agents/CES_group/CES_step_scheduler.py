2f2081cecaeeaa8acc58b093daaf1c7106edb816
root <root@Dizpwnl.>
2025-01-04 10:06:54 +0800

    25-1-4

--- agents/cap_group_agents/CES_group/CES_step_scheduler.py
+++ agents/cap_group_agents/CES_group/CES_step_scheduler.py
@@ -1,4 +1,5 @@
 from agency_swarm import Agent
+from agents.cap_group_agents.step_scheduler_instruction import step_scheduler_instruction
 from agents.tools.read_json_file.ReadJsonFile import ReadJsonFile
 _name = "CES_step_scheduler"
 
@@ -29,21 +30,7 @@ _output_format = """
 }
 """
 
-_instruction = f"""
-作为调度者，你将接收到step流程和初始用户请求，输入格式如下:  
-你将从task_planner那里收到一个 JSON 格式的step规划结果 <plan_graph> 和总任务描述 <main_task>。
-输入格式为:
-{_input_format}
-
-你需要从completed_steps.json中读取已完成的step，从context.json中读取之前step完成过程中的上下文信息，并结合总任务描述一步步思考接下来需要完成的step，保证推进总任务的完成
-
-你需要根据已完成step和上下文信息选出下一步可执行的所有step，确保它们可以**并行执行**；如果两个step可以同时开始执行而彼此不冲突，则可以并行执行。
-
-你的最终调度结果应该为: 
-{_output_format}
-
-你需要在"reason"字段填入你选出这些step的原因
-"""
+_instruction = step_scheduler_instruction(_input_format, _output_format)
 
 _tools = [ReadJsonFile]
 
