0e26e505c0acc0ed30345f26cf53aba1876232a5
A-Failure <37296690+A-Failure@users.noreply.github.com>
2025-01-15 16:12:55 +0800

    修改inspector格式，处理requestAPI问题
    原因是可用区输入错误

--- agents/cap_group_agents/IMS_group/cap_agents/IMS_agent/IMS_agent.py
+++ agents/cap_group_agents/IMS_group/cap_agents/IMS_agent/IMS_agent.py
@@ -1,9 +1,11 @@
 from agency_swarm import Agent
-from agents.cap_group_agents.IMS_group.cap_agents.IMS_agent.tools import (
-    ReadLog, WriteLog
-)
+from agents.cap_group_agents.IMS_group.cap_agents.IMS_agent.tools import ReadAPI
+
+from agents.cap_group_agents.cap_agent_instruction import cap_agent_instruction
 
 _name = "IMS_agent"
+
+_manager_name = "IMS_manager"
 
 _description = """
 负责华为云镜像资源管理任务，包括：查询镜像列表，更新镜像信息，制作镜像，镜像文件快速导入，使用外部镜像文件制作数据镜像，制作整机镜像，注册镜像，导出镜像，查询镜像支持的OS列表。
@@ -12,9 +14,9 @@ _description = """
 import os
 
 current_path = os.path.abspath(os.path.dirname(__file__))
-_instruction = current_path + "/instructions.md"
+_instruction = cap_agent_instruction(_name, _description, _manager_name)
 
-_tools = [ReadLog.ReadLog, WriteLog.WriteLog]
+_tools = [ReadAPI.ReadAPI]
 
 _file_folder = ""
 
