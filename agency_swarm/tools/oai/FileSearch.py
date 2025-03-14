8a05056cac3deb3816cfa0ab6645dfbb2ed05d86
Arsenii Shatokhin <arseny9795@gmail.com>
2025-03-14 11:18:43 +0400

    Prevent file search errors when openai sends incorrect schema

--- agency_swarm/tools/oai/FileSearch.py
+++ agency_swarm/tools/oai/FileSearch.py
@@ -1,5 +1,7 @@
+from typing import Any
 from openai.types.beta.file_search_tool import FileSearch as OpenAIFileSearch
 from openai.types.beta.file_search_tool import FileSearchTool
+from pydantic import ValidationError
 
 
 class FileSearchConfig(OpenAIFileSearch):
@@ -7,4 +9,11 @@ class FileSearchConfig(OpenAIFileSearch):
 
 
 class FileSearch(FileSearchTool):
+    def __init__(self, **data: Any) -> None:
+        try:
+            super().__init__(**data)
+        except ValidationError:
+            # sometimes openai API sends a different schema
+            super().__init__(type="file_search")
+
     type: str = "file_search"
