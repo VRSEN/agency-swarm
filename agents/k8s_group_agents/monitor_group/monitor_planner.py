b8a7127cf3e59d2d44c4ca05d59e27f3eb93b60c
Vivian <990541984@qq.com>
2025-04-27 10:12:07 +0800

    增加监控能力群，增加软件管理能力群

---
+++ agents/k8s_group_agents/monitor_group/monitor_planner.py
@@ -0,0 +1,59 @@
+from agency_swarm import Agent
+from agents.k8s_group_agents.planner_instruction import planner_instruction
+from agents.tools.read_json_file.ReadJsonFile import ReadJsonFile
+
+_name = "monitor_planner"
+
+_description = """
+负责监控能力群的步骤规划
+"""
+
+_group_name = "监控能力群"
+
+_input_format = """
+{
+    "total_subtask_graph": <所有子任务的规划图>,
+    "title": <任务名称>,
+    "description": <任务描述>,
+}
+"""
+
+# TODO
+_agents = """
+1. **monitor_configuration_agent**: {{负责修改k8s集群的插件或配置文件。}}。
+2. **monitor_observe_agent**: {{负责对k8s集群的健康、监控、日志（LTS）、告警（AOM）进行观测。}}。
+"""
+
+_output_format = """
+{
+    "step_1": {
+        "title": 步骤名称,
+        "id": 步骤ID, 
+        "agent": [agent_name_1, ...],
+        "description": 步骤描述, 
+        "dep": <前置步骤ID列表>,
+    },
+    ...
+}
+"""
+
+_instruction = planner_instruction(_group_name, _input_format, _agents, _output_format)
+
+
+_tools = [ReadJsonFile]
+
+_file_folder = ""
+
+def create_agent(*, 
+                 description=_description, 
+                 instuction=_instruction, 
+                 tools=_tools, 
+                 files_folder=_file_folder):
+    return Agent(name=_name,
+                 tools=tools,
+                 description=description,
+                 instructions=instuction,
+                 files_folder=_file_folder,
+                 temperature=0.5,
+                 response_format='auto',
+                 max_prompt_tokens=25000,)
---
+++ agents/k8s_group_agents/software_manage_group/software_manage_planner.py
@@ -0,0 +1,60 @@
+from agency_swarm import Agent
+from agents.k8s_group_agents.planner_instruction import planner_instruction
+from agents.tools.read_json_file.ReadJsonFile import ReadJsonFile
+
+_name = "monitor_planner"
+
+_description = """
+负责软件管理能力群的步骤规划
+"""
+
+_group_name = "软件管理能力群"
+
+_input_format = """
+{
+    "total_subtask_graph": <所有子任务的规划图>,
+    "title": <任务名称>,
+    "description": <任务描述>,
+}
+"""
+
+# TODO
+_agents = """
+1. **software_config_modify_agent**: {{负责对k8s集群中的软件配置进行修改。}}。
+2. **software_install_agent**: {{负责进行k8s集群中软件的安装。}}。
+3. **software_monitor_agent**: {{负责对k8s集群中的软件进行监控。}}。
+"""
+
+_output_format = """
+{
+    "step_1": {
+        "title": 步骤名称,
+        "id": 步骤ID, 
+        "agent": [agent_name_1, ...],
+        "description": 步骤描述, 
+        "dep": <前置步骤ID列表>,
+    },
+    ...
+}
+"""
+
+_instruction = planner_instruction(_group_name, _input_format, _agents, _output_format)
+
+
+_tools = [ReadJsonFile]
+
+_file_folder = ""
+
+def create_agent(*, 
+                 description=_description, 
+                 instuction=_instruction, 
+                 tools=_tools, 
+                 files_folder=_file_folder):
+    return Agent(name=_name,
+                 tools=tools,
+                 description=description,
+                 instructions=instuction,
+                 files_folder=_file_folder,
+                 temperature=0.5,
+                 response_format='auto',
+                 max_prompt_tokens=25000,)
