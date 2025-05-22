cfc63947527fdf9d9a0486831a8beff6ea18005c
zhec <zhecfy@outlook.com>
2025-05-22 18:13:08 +0800

    添加代码scheduler，修改日志

--- agents/k8s_group_agents/planner_instruction.py
+++ agents/k8s_group_agents/planner_instruction.py
@@ -6,7 +6,7 @@ def planner_instruction(_group_name, _input_format, _agents, _output_format):
 
 其中，"title"和"description"字段描述了本次需要规划的子任务，"total_subtask_graph"将描述所有subtask的规划图，包括子任务信息和依赖关系。你接下来对本subtask中各个step的规划不要与其它的subtask冲突或重叠。
 
-同时，你需要先通过`ReadJsonFile`从context_index.json中读取已有环境中的上下文信息。（直接调用工具，不要把它规划为一个step）
+同时，你需要先通过`ReadJsonFile`从completed_sub_tasks.json读取已完成的子任务，从context_index.json中读取已完成任务产生的环境信息。（直接调用工具，不要把它规划为一个step）
 
 获得以上信息后，请严谨专业地一步步思考: 完成该任务需要哪些步骤(step)，每个步骤分别需要哪个能力Agent来操作。
 
@@ -18,15 +18,13 @@ def planner_instruction(_group_name, _input_format, _agents, _output_format):
 你应该按照以下JSON格式进行步骤规划: 
 {_output_format}
 
-对于每个步骤，你需要在 "id" 字段中以"step_正整数"的形式为其分配一个单独的步骤ID，并在"agent"字段填入完成该步骤所需的所有能力agent名称列表 (注意**agent名称列表不应该为空，即每个step都至少需要一个agent**，所有用到的能力agent应该都在你能力范围之内)，并在 "description" 字段中描述步骤内容，并在 "dep" 字段中写入该步骤需要完成的前置步骤 ID 列表（如果没有前置步骤，则写入 []）。
+对于每个step，你需要在 "id" 字段中以"step_正整数"的形式为其分配一个单独的step ID，并在"agent"字段填入完成该step所需的所有能力agent名称列表 (注意**agent名称列表不应该为空，即每个step都至少需要一个agent**，所有用到的能力agent应该都在你能力范围之内)，并在 "description" 字段中描述step内容，并在 "dep" 字段中写入该step依赖的前置step ID 列表（如果没有前置step，则写入 []）。
 
 请逐步思考，综合考虑完成此步骤所需的步骤。
 
 # 注意：你不允许调用`multi_tool_use.parallel`；
 
-# 注意：拆分后的每个步骤完成过程中都不能半途终止；
-
-# 注意：**关注执行核心任务**，非必要时不需要确认信息是否正确、验证命令执行结果等；
+# 注意：**关注执行核心任务**，非必要时不需要确认信息是否正确、验证命令执行结果等。不要设置用户输入中未提到的配置项。
 
 # 注意，你只能考虑**你的能力群内**包含的Agent。
 """
