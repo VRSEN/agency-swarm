ccc2d2e22736da6a80d7cdc34d7dfc486cb951ca
Ryan <ryanmeyersweb@gmail.com>
2025-06-12 11:46:29 -0500

    feature: allow for unit test files within tools folder without causing total meltdown

--- agency_swarm/agents/agent.py
+++ agency_swarm/agents/agent.py
@@ -587,7 +587,11 @@ class Agent:
         if os.path.isdir(self.tools_folder):
             f_paths = os.listdir(self.tools_folder)
             f_paths = [
-                f for f in f_paths if not f.startswith(".") and not f.startswith("__")
+                f
+                for f in f_paths
+                if not f.startswith(".")
+                and not f.startswith("__")
+                and not f.endswith("_test.py")
             ]
             f_paths = [os.path.join(self.tools_folder, f) for f in f_paths]
             for f_path in f_paths:
