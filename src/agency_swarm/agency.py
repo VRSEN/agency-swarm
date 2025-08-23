c9c7b54bbee85bc9932db34bba704cbbe7d1c5d9
ArtemShatokhin <art3m_sh@mail.ru>
2025-08-23 11:59:25 +0400

    Fixed a bug in a tool check

--- src/agency_swarm/agency.py
+++ src/agency_swarm/agency.py
@@ -568,8 +568,8 @@ class Agency:
                     )
 
                     try:
-                        if isinstance(effective_tool_class, SendMessageHandoff) or issubclass(
-                            effective_tool_class, SendMessageHandoff
+                        if isinstance(effective_tool_class, SendMessageHandoff) or (
+                            isinstance(effective_tool_class, type) and issubclass(effective_tool_class, SendMessageHandoff)
                         ):
                             handoff_instance = SendMessageHandoff().create_handoff(recipient_agent=recipient_agent)
                             agent_instance.handoffs.append(handoff_instance)
